982fd68ee8798073d092ae088a38b917930e024a
Daniel <daniel_ciecior@yahoo.de>
2025-07-15 12:03:25 +0200

    admin view and initial List

--- src/Controllers/admin/adminController.ts
+++ src/Controllers/admin/adminController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
+import { getAllBlogPosts } from "../../models/blogPostsModel";
+import { readyData } from "../../util/readyData";
 
-export const adminController = (req: Request, res: Response) => {
-  res.render("../views/admin/index.html", {});
+export const adminController = async (req: Request, res: Response) => {
+  const blogPosts = await getAllBlogPosts();
+  const posts = readyData(blogPosts);
+
+  res.render("../views/admin/index.html", { posts });
 };
