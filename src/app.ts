75b22a742f7401f17f00b81dfe7716e777547d1c
Daniel <daniel_ciecior@yahoo.de>
2025-07-16 10:18:55 +0200

    unescape filter to see tinymce html on post page

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,28 @@ import adminRoutes from "./routes/adminRoutes";
 import { basicAuth } from "./middlewares/basicAuth";
 
 const app = express();
-nunjucks.configure("src/views", {
+const nunjucksEnv = nunjucks.configure("src/views", {
   autoescape: true,
   express: app,
 });
+
+nunjucksEnv.addFilter("unescape", function (str) {
+  const replaceArray = (subject: string, find: string[], replace: string[]) => {
+    let replaceString = subject;
+    var regex;
+    for (var i = 0; i < find.length; i++) {
+      regex = new RegExp(find[i], "g");
+      replaceString = replaceString.replace(regex, replace[i]);
+    }
+    return replaceString;
+  };
+
+  let find = ["&amp;", "&gt;", "&lt;", "&quot;", "&#39;", "&#x2F;"];
+  let replace = ["&", ">", "<", '"', "'", "/"];
+
+  return replaceArray(str, find, replace);
+});
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
