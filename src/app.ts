d3d0b9d8cf38ebdce29d77bfb88b851d658c8f8a
Daniel <daniel_ciecior@yahoo.de>
2025-07-18 10:13:57 +0200

    database implemented preparation

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import publicRoutes from "./routes/publicRoutes";
 import adminRoutes from "./routes/adminRoutes";
 import { basicAuth } from "./middlewares/basicAuth";
 import { unescape } from "./nunjucksFilter/unescape";
+import { connectDB } from "./db/database";
 
 const app = express();
 const nunjucksEnv = nunjucks.configure("src/views", {
@@ -20,11 +21,13 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(publicRoutes);
+connectDB().then(() => {
+  app.use(publicRoutes);
 
-app.use(basicAuth);
-app.use("/admin", adminRoutes);
+  app.use(basicAuth);
+  app.use("/admin", adminRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
+  app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`);
+  });
 });
